30a9cc7c587d08ebc017cf999a6ccc305adaf6c2
Facundo Recabarren <facundorecabarren10@gmail.com>
2023-10-31 13:00:29 -0300

    Cards en la home y link para detalle de producto listos

--- frontend/sonidos_reservados/src/components/Cards.jsx
+++ frontend/sonidos_reservados/src/components/Cards.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import { data } from '../data';
+import { GlobalContext } from '../components/utils/global_context';
 
 const shuffleArray = (array) => {
   const shuffledArray = [...array];
@@ -12,13 +13,15 @@ const shuffleArray = (array) => {
 };
 
 const Cards = () => {
+
+  const { productos } = useContext(GlobalContext);
   const [shuffledData, setShuffledData] = useState([]);
 
   useEffect(() => {
-    const shuffled = shuffleArray(data);
+    const shuffled = shuffleArray(productos);
     const firstTenRandom = shuffled.slice(0, 10);
     setShuffledData(firstTenRandom);
-  }, []);
+  }, [productos]);
 
   return (
     <Recomendaciones>
@@ -26,14 +29,16 @@ const Cards = () => {
         <h2>RECOMENDACIONES</h2>
             <div className='container-items'>
                 {shuffledData.map(product => (
-                <div className='item' key={product.id}>
-                    <figure>
-                        <img src={product.image} alt={product.title} height="400px"/>
-                    </figure>
-                    <div className='info-product'>
-                        <h3>{product.title}</h3>
-                    </div>
-                </div>
+                <Link className='link' to={`/details/${product.id}`}>
+                  <div className='item' key={product.id}>
+                      <figure>
+                          <img src={product.image} alt={product.title} className='cardImage'/>
+                      </figure>
+                      <div className='info-product'>
+                          <h3>{product.title}</h3>
+                      </div>
+                  </div>
+                </Link>
                 ))}
             </div>
         </div>
@@ -56,11 +61,13 @@ const Recomendaciones = styled.div`
         font-family: 'Concert One', sans-serif;
         font-size: 2rem;
     }
+    .link{
+      text-decoration: none;
+    }
     .container-items{
         display: flex;
         flex-wrap: wrap;
         justify-content: space-between;
-
     }
     .item{
         border-radius: 20px;
@@ -70,6 +77,8 @@ const Recomendaciones = styled.div`
         flex: 0 0 calc(50% - 50px);
         margin: 5px;
         cursor: pointer;
+        width: 600px;
+        height: 600px;
     }
     h3{
         margin: 2rem;
@@ -79,7 +88,10 @@ const Recomendaciones = styled.div`
         font-family: 'Concert One', sans-serif;
     }
 
+    .cardImage{
+      height: 400px;
+      width: 400px;
+      object-fit: cover;
+    }
     
-
-
 `
